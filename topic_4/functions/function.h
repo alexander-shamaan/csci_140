f837a55a9d44938acd05ff3c7ca197d3cfe52721
ashamaan <alexander.shamaan@gmail.com>
2025-03-30 19:11:36 -0700

    Topic 4

--- topic_4/functions/function.h
+++ topic_4/functions/function.h
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define WINDOWS true 
+    
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
@@ -8,6 +10,12 @@ void printString(const std::string&, int ); //print x amount of times
 
 void swap(int& , int&);
 
+#if WINDOWS
+void printInt(int);
+void printInt();
 
+//void printInt(int = 10);
+
+#endif //WINDOWS
 
 #endif
